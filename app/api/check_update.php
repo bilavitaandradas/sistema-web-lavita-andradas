b055bdfa113702c767b108364bc42c1cb4020b36
EDUARDO ALMEIDA CAMPOS SILVA <ecsilva@lavita.local>
2026-06-30 08:24:52 -0300

    Atualiza check_update para versão 1.0.2

--- app/api/check_update.php
+++ app/api/check_update.php
@@ -3,8 +3,8 @@
 header('Content-Type: application/json');
 
 echo json_encode([
-    'version' => '1.0.1',
-    'apk_url' => 'http://201.20.62.200/TCC/app/update/apk/tcc_app_v1.0.1.apk',
+    'version' => '1.0.2',
+    'apk_url' => 'http://201.20.62.200/TCC/app/update/apk/tcc_app_v1.0.2.apk',
     
     'force' => true
 ]);
