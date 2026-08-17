fc1aaaa978f26444ed216c622b4095f24621c488
EDUARDO ALMEIDA CAMPOS SILVA <ecsilva@lavita.local>
2026-08-17 10:15:01 -0300

    Atualiza app para versão 1.0.4

--- app/api/check_update.php
+++ app/api/check_update.php
@@ -3,8 +3,8 @@
 header('Content-Type: application/json');
 
 echo json_encode([
-    'version' => '1.0.3',
-    'apk_url' => 'http://201.20.62.200/TCC/app/update/apk/tcc_app_v1.0.3.apk',
+    'version' => '1.0.4',
+    'apk_url' => 'http://201.20.62.200/TCC/app/update/apk/tcc_app_v1.0.4.apk',
     
     'force' => true
 ]);
